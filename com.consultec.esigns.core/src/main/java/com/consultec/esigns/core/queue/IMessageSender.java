44f646084c51e46ab1c2a8b306b33f962f40c481
Hector Rodriguez <hrodriguez@consultec-ti.com>
2019-08-06 12:11:33 -0500

    se resuelve manejo de excepciones para capturar correctamente los eventos al momento de cerrar el editor de PDF

--- com.consultec.esigns.core/src/main/java/com/consultec/esigns/core/queue/IMessageSender.java
+++ com.consultec.esigns.core/src/main/java/com/consultec/esigns/core/queue/IMessageSender.java
@@ -3,17 +3,21 @@
  */
 package com.consultec.esigns.core.queue;
 
+import java.net.ConnectException;
+
+import javax.jms.JMSException;
+
 /**
  * The Interface IMessageSender.
  *
  * @author hrodriguez
  */
 public interface IMessageSender {
-	
-	/**
-	 * Send message.
-	 *
-	 * @param message the message
-	 */
-	void sendMessage(String message);
+
+  /**
+   * Send message.
+   *
+   * @param message the message
+   */
+  void sendMessage(String message) throws JMSException, ConnectException;
 }
